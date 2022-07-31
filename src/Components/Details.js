cd95eff4a2cd586eb410b1f0c74a4efea04ac5a1
trance <michaelmunavu83@gmail.com>
2022-07-31 19:59:57 +0300

    Added link to Details js

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Details() {
   return (
-    <div>Details</div>
+    <div>
+        Details
+        <Link to="/">back</Link>
+
+    </div>
   );
 }
 
