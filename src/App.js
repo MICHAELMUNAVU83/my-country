5f9309f117703fdfee30c6dce22b4b373786d7c6
trance <michaelmunavu83@gmail.com>
2022-07-31 19:58:18 +0300

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Countries from './Components/Countries';
 import Details from './Components/Details';
+import {BrowserRouter as Router , Routes , Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Countries />
-      <Details />
+      <Router>
+        <Routes>
+          <Route path ="/" element={ <Countries />} />
+          <Route path="/details" element={  <Details />} />
+        </Routes>
+      </Router>
+     
+    
     </div>
   );
 }
