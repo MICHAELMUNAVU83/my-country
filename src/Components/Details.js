9ab4dab757696cf33fa2ce488334a5a52ca5404b
trance <michaelmunavu83@gmail.com>
2022-08-01 12:01:58 +0300

    Fixed linters and showed details for each

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
+import { FaChevronCircleLeft } from 'react-icons/fa';
 import { getCountriesApi } from '../redux/countries';
 
 function Details() {
@@ -14,9 +15,37 @@ function Details() {
 
   return (
     <div>
-      {countries[index].name.common}
-      <Link to="/">back</Link>
-
+      <img
+        className="card-img-top"
+        id="coat"
+        src={countries[index].coatOfArms.png}
+        alt="Card cap"
+      />
+      <p className="card-text">
+        COUNTRY :
+        {countries[index].name.common}
+      </p>
+      <p className="card-text">
+        CONTINENT :
+        {countries[index].continents}
+      </p>
+      <p className="card-text">
+        POPULATION :
+        {countries[index].population}
+      </p>
+      <p className="card-text">
+        CAPITAL :
+        {countries[index].capital}
+      </p>
+      <p className="card-text">
+        AREA :
+        {countries[index].area}
+      </p>
+      <p className="card-text">
+        Timezone :
+        {countries[index].timezones}
+      </p>
+      <Link to="/"><FaChevronCircleLeft /></Link>
     </div>
   );
 }
